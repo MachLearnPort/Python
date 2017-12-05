43fb5f4743367cb8338d741a4ac4b31b9022c718
MachLearnPort <jeff_schwartzentruber@hotmail.com>
2017-12-04 22:01:37 -0500

    Scrapper needs mods to work
    API change from yahoo

--- sp500_scrapper.py
+++ sp500_scrapper.py
@@ -21,9 +21,10 @@ def save_sp500_tickers(): #function for getting the ticker names
         
     return tickers
 
-print(save_sp500_tickers())
+#print(save_sp500_tickers())
 
 def get_data_from_yahoo(reload_sp500=False): #function for getting the ticker data
+  nowork=[]
   if reload_sp500: 
     tickers = save_sp500_tickers()
   else:
@@ -35,16 +36,44 @@ def get_data_from_yahoo(reload_sp500=False): #function for getting the ticker da
 
     #set start and end time for loop
   start = dt.datetime(2000,1,1)
-  end = dt.datetime(2017,12,31)
+  today = dt.datetime.today()
+  end = dt.datetime(today.year, today.month, today.day)
   
   for ticker in tickers: # you could use tickers[], and inside the brackets, say what tickers you want
     if not os.path.exists('stock_dfs/{}.csv'.format(ticker)):
       fix_yahoo_finance.pdr_override() #had to add to fix the yahoo issue with cookies
       df = web.get_data_yahoo(ticker, start, end)
       df.to_csv('stock_dfs/{}.csv'.format(ticker))
+    if os.path.getsize("stock_dfs/{}.csv".format(ticker)) <= 3:      
+      nowork.append(ticker)       
     else:
       print('Already have {}'. format(ticker))
-      
+  print(nowork) 
+
 get_data_from_yahoo()
 
+def compile_data():
+    with open("sp500tickers.pickle","rb") as f:
+        tickers = pickle.load(f)
+
+    main_df = pd.DataFrame()
     
+    for count,ticker in enumerate(tickers):
+        df = pd.read_csv('stock_dfs/{}.csv'.format(ticker))
+        df.set_index('Date', inplace=True)
+
+        df.rename(columns={'Adj Close':ticker}, inplace=True)
+        df.drop(['Open','High','Low','Close','Volume'],1,inplace=True)
+
+        if main_df.empty:
+            main_df = df
+        else:
+            main_df = main_df.join(df, how='outer')
+
+        if count % 10 == 0:
+            print(count)
+    print(main_df.head())
+    main_df.to_csv('sp500_joined_closes.csv')
+
+
+compile_data()
